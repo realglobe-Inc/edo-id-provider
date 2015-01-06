4ad3d1a0f66311cc37541de200a71d7028c2b0af
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-01-06 10:05:05 +0900

    TA 型を ta.go として ta_container.go から分離

---
+++ ta.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"crypto"
+	"github.com/realglobe-Inc/edo/util"
+)
+
+type ta struct {
+	id   string
+	name string
+	// 登録された全ての redirect_uri。
+	rediUris *util.StringSet
+	// kid から公開鍵へのマップ。
+	pubKeys map[string]crypto.PublicKey
+}
+
+func (this *ta) hasRedirectUri(rediUri string) bool {
+	return this.rediUris.Contains(rediUri)
+}
+
+func (this *ta) keys() map[string]crypto.PublicKey {
+	return this.pubKeys
+}
--- ta_container.go
+++ ta_container.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"crypto"
 	"github.com/realglobe-Inc/edo/driver"
-	"github.com/realglobe-Inc/edo/util"
 	"github.com/realglobe-Inc/go-lib-rg/erro"
 )
-
-type ta struct {
-	id   string
-	name string
-	// 登録された全ての redirect_uri。
-	rediUris *util.StringSet
-	// kid から公開鍵へのマップ。
-	pubKeys map[string]crypto.PublicKey
-}
-
-func (this *ta) hasRedirectUri(rediUri string) bool {
-	return this.rediUris.Contains(rediUri)
-}
 
 type taContainer interface {
 	get(taId string) (*ta, error)

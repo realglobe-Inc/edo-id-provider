2f279ba1b0afde2aa2191f428b0b998bcd49eb61
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-01-07 18:27:39 +0900

    関数を使うようにした

--- ta_container_file.go
+++ ta_container_file.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"crypto"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rsa"
-	"encoding/base64"
 	"encoding/json"
 	"github.com/realglobe-Inc/edo/driver"
 	"github.com/realglobe-Inc/edo/util"
 	"github.com/realglobe-Inc/go-lib-rg/erro"
-	"math/big"
 	"net/url"
 	"strings"
 	"time"
@@ -31,42 +26,7 @@ func taToIntermediate(t *ta) *taIntermediate {
 	rediUris := t.rediUris
 	pubKeys := []map[string]interface{}{}
 	for k, v := range t.pubKeys {
-		switch pubKey := v.(type) {
-		case *rsa.PublicKey:
-			pubKeys = append(pubKeys, map[string]interface{}{
-				"kty": "RSA",
-				"n":   base64.URLEncoding.EncodeToString(pubKey.N.Bytes()),
-				"e":   base64.URLEncoding.EncodeToString(big.NewInt(int64(pubKey.E)).Bytes()),
-				"kid": k,
-			})
-		case *ecdsa.PublicKey:
-			s := pubKey.Params().BitSize
-			m := map[string]interface{}{}
-			switch s {
-			case 256:
-				m["crv"] = "P-256"
-			case 384:
-				m["crv"] = "P-384"
-			case 521:
-				m["crv"] = "P-521"
-			}
-			if len(m) == 0 {
-				break // switch pubKey
-			}
-			byteLen := (s + 7) / 8
-			x, y := pubKey.X.Bytes(), pubKey.Y.Bytes()
-			if len(x) < byteLen {
-				buff := make([]byte, byteLen)
-				copy(buff[byteLen-len(x):], x)
-				x = buff
-			}
-			if len(y) < byteLen {
-				buff := make([]byte, byteLen)
-				copy(buff[byteLen-len(y):], y)
-				y = buff
-			}
-			m["x"] = base64.URLEncoding.EncodeToString(x)
-			m["y"] = base64.URLEncoding.EncodeToString(y)
+		if m := util.EncodePublicKeyToJwkMap(k, v); m != nil {
 			pubKeys = append(pubKeys, m)
 		}
 	}
@@ -81,7 +41,7 @@ func taToIntermediate(t *ta) *taIntermediate {
 func intermediateToTa(ti *taIntermediate) (*ta, error) {
 	pubKeys := map[string]crypto.PublicKey{}
 	for _, v := range ti.PubKeys {
-		if kid, pubKey, err := mapToPublicKey(v); err != nil {
+		if kid, pubKey, err := util.ParsePublicKeyFromJwkMap(v); err != nil {
 			return nil, erro.Wrap(err)
 		} else if pubKey != nil {
 			pubKeys[kid] = pubKey
@@ -105,71 +65,6 @@ func unmarshalTa(data []byte) (interface{}, error) {
 		return nil, erro.Wrap(err)
 	}
 	return intermediateToTa(&ti)
-}
-
-// 公開鍵を中間形式から戻す。
-// 中間形式は以下のような JWK を json.Unmarshal したものだけに対応。
-// {
-//     "kty": "RSA",
-//     "n": "XXX...",
-//     "e": "YYY",
-//     "kid": "ZZZ"
-// }
-func mapToPublicKey(m map[string]interface{}) (kid string, pubKey crypto.PublicKey, err error) {
-	switch kty, _ := m["kty"].(string); kty {
-	case "":
-		return "", nil, nil
-	case "RSA":
-		var buff rsa.PublicKey
-		if nStr, _ := m["n"].(string); nStr == "" {
-			return "", nil, erro.New("no n")
-		} else if nRaw, err := base64.URLEncoding.DecodeString(nStr); err != nil {
-			return "", nil, erro.Wrap(err)
-		} else {
-			buff.N = (&big.Int{}).SetBytes(nRaw)
-		}
-		if eStr, _ := m["e"].(string); eStr == "" {
-			return "", nil, erro.New("no e")
-		} else if eRaw, err := base64.URLEncoding.DecodeString(eStr); err != nil {
-			return "", nil, erro.Wrap(err)
-		} else {
-			buff.E = int((&big.Int{}).SetBytes(eRaw).Int64())
-		}
-		kid, _ = m["kid"].(string)
-		return kid, &buff, nil
-	case "EC":
-		var buff ecdsa.PublicKey
-		switch crv, _ := m["crv"].(string); crv {
-		case "":
-			return "", nil, erro.New("no crv")
-		case "P-256":
-			buff.Curve = elliptic.P256()
-		case "P-384":
-			buff.Curve = elliptic.P384()
-		case "P-521":
-			buff.Curve = elliptic.P521()
-		default:
-			return "", nil, erro.New("unsupported elliptic curve " + crv)
-		}
-		if xStr, _ := m["x"].(string); xStr == "" {
-			return "", nil, erro.New("no x")
-		} else if xRaw, err := base64.URLEncoding.DecodeString(xStr); err != nil {
-			return "", nil, erro.Wrap(err)
-		} else {
-			buff.X = (&big.Int{}).SetBytes(xRaw)
-		}
-		if yStr, _ := m["y"].(string); yStr == "" {
-			return "", nil, erro.New("no y")
-		} else if yRaw, err := base64.URLEncoding.DecodeString(yStr); err != nil {
-			return "", nil, erro.Wrap(err)
-		} else {
-			buff.Y = (&big.Int{}).SetBytes(yRaw)
-		}
-		kid, _ = m["kid"].(string)
-		return kid, &buff, nil
-	default:
-		return "", nil, nil
-	}
 }
 
 func keyToJsonPath(key string) string {

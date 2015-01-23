d96d851c20ec44d697380b9ad15452c232301571
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-01-23 20:16:05 +0900

    セッション型の更新日時を使うようにした

--- session_container_redis.go
+++ session_container_redis.go
@@ -4,11 +4,18 @@ import (
 	"encoding/json"
 	"github.com/garyburd/redigo/redis"
 	"github.com/realglobe-Inc/edo/driver"
+	"strconv"
 	"time"
 )
 
+func getSessionStamp(val interface{}) *driver.Stamp {
+	sess, _ := val.(*session)
+	upd := sess.updateDate()
+	return &driver.Stamp{Date: upd, Digest: strconv.FormatInt(upd.UnixNano(), 16)}
+}
+
 func newRedisSessionContainer(minIdLen int, pool *redis.Pool, tag string, caStaleDur, caExpiDur time.Duration) sessionContainer {
 	return newSessionContainerImpl(
-		driver.NewRedisTimeLimitedKeyValueStore(pool, tag+":", json.Marshal, unmarshalSession, getDummyStamp, caStaleDur, caExpiDur),
+		driver.NewRedisTimeLimitedKeyValueStore(pool, tag+":", json.Marshal, unmarshalSession, getSessionStamp, caStaleDur, caExpiDur),
 		minIdLen)
 }

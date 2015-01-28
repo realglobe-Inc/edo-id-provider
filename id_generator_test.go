9807c6156c6a8b50d9bd825f6149d5cf0aa501ed
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-01-28 12:59:37 +0900

    ID 生成器の並列使用テストを追加

--- id_generator_test.go
+++ id_generator_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -23,5 +24,66 @@ func TestIdGenerator(t *testing.T) {
 			}
 			m[id] = true
 		}
+	}
+}
+
+func TestIdGeneratorConcurrent(t *testing.T) {
+	const (
+		p = 10
+		n = 5000
+	)
+
+	idGen := newIdGenerator(0)
+
+	glbl := map[string]bool{}
+	var lock sync.Mutex
+	errCh := make(chan int, p)
+	endCh := make(chan struct{}, p)
+
+	for j := 0; j < p; j++ {
+		go func(self int) {
+			defer func() { endCh <- struct{}{} }()
+
+			// まずローカルで ID 生成する。
+			buff := map[string]bool{}
+			for i := 0; i < n; i++ {
+				id, err := idGen.newId()
+				if err != nil {
+					t.Error(err)
+					errCh <- self
+					return
+				} else if buff[id] {
+					errCh <- self
+					return
+				}
+				buff[id] = true
+			}
+
+			// グローバルに ID が重複していないか検査。
+			lock.Lock()
+			defer lock.Unlock()
+			for id := range buff {
+				if glbl[id] {
+					errCh <- self
+					return
+				}
+				glbl[id] = true
+			}
+		}(j)
+	}
+
+	for i := 0; i < p; i++ {
+		<-endCh
+	}
+	for ok := false; ok; {
+		select {
+		case pid := <-errCh:
+			t.Error(pid)
+		default:
+			ok = true
+		}
+	}
+	if len(glbl) != p*n {
+		t.Error(len(glbl), p*n)
 	}
 }

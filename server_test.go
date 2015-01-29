9692c27da21ad56698aa8ddceea582ba4cd8afaf
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-01-29 16:33:57 +0900

    トークンリクエストが POST ではないときに拒否できるかどうかのテストを追加

--- server_test.go
+++ server_test.go
@@ -992,6 +992,119 @@ func TestDirectErrorResponseInNoRedirectUri(t *testing.T) {
 	}
 }
 
+// POST でないトークンリクエストを拒否できるか。
+func TestDenyNonPostTokenRequest(t *testing.T) {
+	// ////////////////////////////////
+	// util.SetupConsoleLog("github.com/realglobe-Inc", level.ALL)
+	// defer util.SetupConsoleLog("github.com/realglobe-Inc", level.OFF)
+	// ////////////////////////////////
+
+	testTa2, rediUri, kid, sigKey, taServ, idpSys, shutCh, err := setupTestTaAndIdp(nil, []*account{testAcc}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer taServ.Close()
+	defer os.RemoveAll(idpSys.uiPath)
+	defer func() { shutCh <- struct{}{} }()
+
+	// サーバ起動待ち。
+	time.Sleep(10 * time.Millisecond)
+
+	for _, meth := range []string{"GET", "PUT"} {
+		// TA にリダイレクトできたときのレスポンスを設定しておく。
+		taServ.AddResponse(http.StatusOK, nil, []byte("success"))
+
+		cookJar, err := cookiejar.New(nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		cli := &http.Client{Jar: cookJar}
+
+		consResp, err := testFromRequestAuthToConsent(idpSys, cli, map[string]string{
+			"scope":         "openid email",
+			"response_type": "code",
+			"client_id":     testTa2.id(),
+			"redirect_uri":  rediUri,
+		}, map[string]string{
+			"username": testAcc.name(),
+		}, map[string]string{
+			"username": testAcc.name(),
+			"password": testAcc.password(),
+		}, map[string]string{
+			"consented_scope": "openid email",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer consResp.Body.Close()
+
+		cod := consResp.Request.FormValue("code")
+		if cod == "" {
+			util.LogRequest(level.ERR, consResp.Request, true)
+			t.Fatal("no code")
+		}
+
+		// 認可コードを取得できた。
+
+		assJws := util.NewJws()
+		assJws.SetHeader("alg", "RS256")
+		assJws.SetHeader("kid", kid)
+		assJws.SetClaim("iss", testTa2.id())
+		assJws.SetClaim("sub", testTa2.id())
+		assJws.SetClaim("aud", idpSys.selfId+"/token")
+		assJws.SetClaim("jti", strconv.FormatInt(time.Now().UnixNano(), 16))
+		assJws.SetClaim("exp", time.Now().Add(idpSys.idTokExpiDur).Unix())
+		assJws.SetClaim("code", cod)
+		if err := assJws.Sign(map[string]crypto.PrivateKey{kid: sigKey}); err != nil {
+			t.Fatal(err)
+		}
+		assBuff, err := assJws.Encode()
+		if err != nil {
+			t.Fatal(err)
+		}
+		ass := string(assBuff)
+
+		req, err := http.NewRequest(meth, idpSys.selfId+"/token", strings.NewReader(url.Values{
+			"grant_type":            {"authorization_code"},
+			"redirect_uri":          {rediUri},
+			"client_id":             {testTa2.id()},
+			"client_assertion_type": {"urn:ietf:params:oauth:client-assertion-type:jwt-bearer"},
+			"code":                  {cod},
+			"client_assertion":      {ass},
+		}.Encode()))
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		resp, err := (&http.Client{}).Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusMethodNotAllowed {
+			util.LogRequest(level.ERR, req, true)
+			util.LogResponse(level.ERR, resp, true)
+			t.Fatal(resp.StatusCode, http.StatusMethodNotAllowed)
+		}
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			util.LogRequest(level.ERR, req, true)
+			util.LogResponse(level.ERR, resp, true)
+			t.Fatal(err)
+		}
+
+		var res struct{ Error string }
+		if err := json.Unmarshal(data, &res); err != nil {
+			util.LogRequest(level.ERR, req, true)
+			util.LogResponse(level.ERR, resp, true)
+			t.Fatal(err)
+		} else if res.Error != errInvReq {
+			t.Fatal(res.Error, errInvReq)
+		}
+	}
+}
+
 // 認証中にエラーが起きたら認証経過を破棄できるか。
 func TestAbortSession(t *testing.T) {
 	// ////////////////////////////////

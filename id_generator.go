339daffad7bec0d6f51cb041351c5c29822dfcd0
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-02-02 13:05:53 +0900

    ID 生成の通し番号部分で使い切れない桁が出ないようにした

--- id_generator.go
+++ id_generator.go
@@ -11,26 +11,39 @@ import (
 )
 
 type idGenerator struct {
-	// 文字数の下界。
-	minIdLen int
+	// 乱数文字数の長さ。
+	randLen int
 	// インスタンス内での ID 被りを防ぐための通し番号。
-	// 別インスタンスでは保証できない。
 	ser int64
 }
 
-func newIdGenerator(minIdLen int) idGenerator {
+func newIdGenerator(randLen int) idGenerator {
 	return idGenerator{
-		minIdLen: minIdLen,
-		ser:      rand.New(rand.NewSource(time.Now().UnixNano())).Int63(),
+		randLen: randLen,
+		ser:     rand.New(rand.NewSource(time.Now().UnixNano())).Int63(),
 	}
 }
 
 func (this *idGenerator) newId() (id string, err error) {
-	id, err = util.SecureRandomString(this.minIdLen)
+	id, err = util.SecureRandomString(this.randLen)
 	if err != nil {
 		return "", erro.Wrap(err)
 	}
-	buff := big.NewInt(atomic.AddInt64(&this.ser, 1)).Bytes()
-	id += base64.URLEncoding.EncodeToString(buff)[:(len(buff)*8+5)/6]
+
+	const bLen = 64 / 8       // int64
+	const sLen = bLen * 8 / 6 // BASE64 にして文字を使い切れない上位ビットは捨てる。
+
+	v := big.NewInt(atomic.AddInt64(&this.ser, 1))
+	v = v.Lsh(v, bLen*8-sLen*6) // BASE64 の 6 ビット区切りと最下位ビットの位置を揃える。
+	buff := v.Bytes()
+	if len(buff) < bLen {
+		// 上位を 0 詰め。
+		buff = append(make([]byte, bLen-len(buff)), buff...)
+	} else if len(buff) > bLen {
+		// 上位を捨てる。
+		buff = buff[len(buff)-bLen:]
+	}
+	id += base64.URLEncoding.EncodeToString(buff)[:sLen]
+
 	return id, nil
 }

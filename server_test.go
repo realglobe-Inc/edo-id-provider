e599a90597e3d370b3a094d12de4f5469cfc3642
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-02-04 12:59:46 +0900

    認証リクエストからトークンリクエストまでやって、最後のレスポンスをそのまま返すテスト用関数を追加

--- server_test.go
+++ server_test.go
@@ -547,6 +547,22 @@ func testFromRequestAuthToConsent(idpSys *system, cli *http.Client,
 	return testConsent(idpSys, cli, loginResp, consParams)
 }
 
+// 認証リクエストからトークンリクエストまでして結果を無検査で返す。
+func testFromRequestAuthToGetTokenWithoutCheck(idpSys *system, cli *http.Client,
+	authParams, selParams, loginParams, consParams map[string]string,
+	assHeads, assClms map[string]interface{}, tokParams map[string]string, kid string, sigKey crypto.PrivateKey) (*http.Response, error) {
+
+	// リクエストから同意までする。
+	consResp, err := testFromRequestAuthToConsent(idpSys, cli, authParams, selParams, loginParams, consParams)
+	if err != nil {
+		return nil, erro.Wrap(err)
+	}
+	defer consResp.Body.Close()
+
+	// アクセストークンを取得してアカウント情報を取得する。
+	return testGetTokenWithoutCheck(idpSys, consResp, assHeads, assClms, tokParams, kid, sigKey)
+}
+
 // トークンリクエストからアカウント情報取得までする。
 func testGetTokenAndAccountInfo(idpSys *system, consResp *http.Response,
 	assHeads, assClms map[string]interface{}, tokParams map[string]string, kid string, sigKey crypto.PrivateKey,

852d97eae84f37c59fb6dfcdb29e4db688b53941
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-02-04 15:43:07 +0900

    発行先違いの認可コードを送ってきたときに拒否できるかどうかのテストを追加

--- api_token_test.go
+++ api_token_test.go
@@ -599,6 +599,85 @@ func TestDenyNoRedirectUriInTokenRequest(t *testing.T) {
 	}
 }
 
+// 認可コードが発行されたクライアントでないなら拒否できるか。
+// クライアントが不正な認可コードを使っているとして error は invalid_grant か。
+func TestDenyNotCodeHolder(t *testing.T) {
+	// ////////////////////////////////
+	// util.SetupConsoleLog("github.com/realglobe-Inc", level.ALL)
+	// defer util.SetupConsoleLog("github.com/realglobe-Inc", level.OFF)
+	// ////////////////////////////////
+
+	taBuff := *testTa
+	taBuff.Id = "http://example.org"
+	testTa1 := &taBuff
+
+	testTa2, rediUri, kid, sigKey, taServ, idpSys, shutCh, err := setupTestTaAndIdp(nil, []*account{testAcc}, []*ta{testTa1})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer taServ.Close()
+	defer os.RemoveAll(idpSys.uiPath)
+	defer func() { shutCh <- struct{}{} }()
+	// TA にリダイレクトできたときのレスポンスを設定しておく。
+	taServ.AddResponse(http.StatusOK, nil, []byte("success"))
+
+	// サーバ起動待ち。
+	time.Sleep(10 * time.Millisecond)
+
+	cookJar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cli := &http.Client{Jar: cookJar}
+
+	resp, err := testFromRequestAuthToGetTokenWithoutCheck(idpSys, cli, map[string]string{
+		"scope":         "openid email",
+		"response_type": "code",
+		"client_id":     testTa2.id(),
+		"redirect_uri":  rediUri,
+	}, map[string]string{
+		"username": testAcc.name(),
+	}, map[string]string{
+		"username": testAcc.name(),
+		"password": testAcc.password(),
+	}, map[string]string{
+		"consented_scope": "openid email",
+	}, map[string]interface{}{
+		"alg": "RS256",
+		"kid": kid,
+	}, map[string]interface{}{
+		"iss": testTa1.id(),
+		"sub": testTa1.id(),
+		"aud": idpSys.selfId + "/token",
+		"jti": strconv.FormatInt(time.Now().UnixNano(), 16),
+		"exp": time.Now().Add(idpSys.idTokExpiDur).Unix(),
+	}, map[string]string{
+		"grant_type":            "authorization_code",
+		"redirect_uri":          rediUri,
+		"client_id":             testTa1.id(),
+		"client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+	}, kid, sigKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusBadRequest {
+		util.LogResponse(level.ERR, resp, true)
+		t.Fatal(resp.StatusCode, http.StatusBadRequest)
+	}
+
+	var res struct{ Error, Error_description string }
+	if data, err := ioutil.ReadAll(resp.Body); err != nil {
+		util.LogResponse(level.ERR, resp, true)
+		t.Fatal(err)
+	} else if err := json.Unmarshal(data, &res); err != nil {
+		util.LogResponse(level.ERR, resp, true)
+		t.Fatal(err)
+	} else if res.Error != errInvGrnt {
+		t.Fatal(res.Error, res.Error_description, errInvGrnt)
+	}
+}
+
 // 認可コードが 2 回使われたら拒否できるか。
 func TestDenyUsedCode(t *testing.T) {
 	// ////////////////////////////////

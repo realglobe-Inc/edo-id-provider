c7c9ef8c487ffb82cc2a0cd86e5dc885757f9c34
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-02-12 17:55:30 +0900

    アカウント選択が必要でもアカウント選択画面を表示できないときにエラーを返せるかどうかのテストを追加

--- page_auth_test.go
+++ page_auth_test.go
@@ -1100,3 +1100,51 @@ func TestForceSelect(t *testing.T) {
 		t.Fatal(resp.Request.URL.Path, idpSys.uiUri+"/select.html")
 	}
 }
+
+// prompt が none と select_account を含むならエラーを返すか。
+func TestForceSelectError(t *testing.T) {
+	// ////////////////////////////////
+	// logutil.SetupConsole("github.com/realglobe-Inc", level.ALL)
+	// defer logutil.SetupConsole("github.com/realglobe-Inc", level.OFF)
+	// ////////////////////////////////
+
+	testTa2, rediUri, _, _, taServ, idpSys, shutCh, err := setupTestTaAndIdp(nil, []*account{testAcc}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer taServ.Close()
+	defer idpSys.close()
+	defer os.RemoveAll(idpSys.uiPath)
+	defer func() { shutCh <- struct{}{} }()
+	// TA にリダイレクトしたときのレスポンスを設定しておく。
+	taServ.AddResponse(http.StatusOK, nil, []byte("success"))
+
+	// サーバ起動待ち。
+	time.Sleep(10 * time.Millisecond)
+
+	cookJar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cli := &http.Client{Jar: cookJar}
+
+	// 認証 UI に飛ばされる。
+	resp, err := testRequestAuthWithoutCheck(idpSys, cli, map[string]string{
+		"scope":         "openid email",
+		"response_type": "code",
+		"client_id":     testTa2.id(),
+		"redirect_uri":  rediUri,
+		"prompt":        "none select_account",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		server.LogResponse(level.ERR, resp, true)
+		t.Fatal(resp.StatusCode, http.StatusOK)
+	} else if q := resp.Request.URL.Query(); q.Get("error") != errAccSelReq {
+		t.Fatal(q.Get("error"), errAccSelReq)
+	}
+}
